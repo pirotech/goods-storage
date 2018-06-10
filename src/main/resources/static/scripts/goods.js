5c4eb6bcd65655e2cfd64fc3bbdaa437383bbb8d
Sergey Chistyakov <serjioini@gmail.com>
2018-06-10 17:04:27 +0300

    Страница /goods отображает данные
    Страница /goods тянет данные о товарах через API. Подключил bootstrap#3.0.0 и AngularJS. Переходим от mustache к thymeleaf.

---
+++ src/main/resources/static/scripts/goods.js
@@ -0,0 +1,13 @@
+var goods = angular.module('goods', []);
+
+var goodsCtrl = goods.controller('goodsCtrl', function ($scope, $http) {
+    $http.get('http://localhost:8080/api/goods/').then(function (response) {
+        $scope.goods = response.data;
+    });
+    $scope.editGoods = function (id) {
+        console.log(id);
+    };
+    $scope.deleteGoods = function (id) {
+        console.log(id);
+    };
+});
