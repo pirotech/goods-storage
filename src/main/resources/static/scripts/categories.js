237684157b5bfba840c5484713c8a596e0628370
Sergey Chistyakov <serjioini@gmail.com>
2018-06-11 20:50:28 +0300

    Через страницу /categories можно редактировать и удалять категории
    После редактировании категория появляется последней, при удалении запрашивается подтверждение.

--- src/main/resources/static/scripts/categories.js
+++ src/main/resources/static/scripts/categories.js
@@ -1,14 +1,54 @@
 var categories = angular.module('categories', []);
 
 var categoriesCtrl = categories.controller('categoriesCtrl', function ($scope, $http) {
-    $http.get('http://localhost:8080/api/categories/').then(function (response) {
-        $scope.categories = response.data != null ? response.data : [];
-        console.log(response.data);
-    });
-    $scope.editCategory = function (id) {
-        console.log(id);
+    // util
+    var l = window.location;
+    $scope.api = l.protocol + '//' + l.host + '/api/categories/';
+
+    // load
+    $scope.loadCategories = function() {
+        $http.get($scope.api).then(function (response) {
+            $scope.categories = response.data != null ? response.data : [];
+        });
     };
-    $scope.deleteCategory = function (id) {
-        console.log(id);
+
+    // edit
+    $scope.editorMode = false;
+    $scope.editCategory = function (category) {
+        $scope.editorMode = true;
+        $scope.edited = category;
     };
+    $scope.saveCategory = function () {
+        $http.patch($scope.api + $scope.edited.id, {
+            name: $('#name' + $scope.edited.id).val(),
+            description: $('#description' + $scope.edited.id).val()
+        }).then(function () {
+            $scope.loadCategories();
+            $scope.editorMode = false;
+        });
+    };
+    $scope.cancelCategory = function () {
+        $scope.editorMode = false;
+    };
+
+    // delete
+    $scope.deleteCategory = function (category) {
+        $scope.deleted = category;
+    };
+    $scope.reallyDeleteCategory = function () {
+        $http.delete($scope.api + $scope.deleted.id).then(function () {
+            $scope.loadCategories();
+        });
+    };
+
+    // create
+    $scope.created = {};
+    $scope.createCategory = function() {
+        $http.post($scope.api, $scope.created).then(function () {
+            $scope.loadCategories();
+            $scope.created = {};
+        });
+    };
+
+    $scope.loadCategories();
 });
